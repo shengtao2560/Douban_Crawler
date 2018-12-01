bfe68ed2b5e448cbb7d71d21b4f6199de43d40a8
shengtao <16307130107@fudan.edu.cn>
2018-12-02 05:59:42 +0800

    final

--- crawler.py
+++ crawler.py
@@ -12,15 +12,21 @@ import os
 import time
 from PIL import Image
 import emoji
+from pylab import mpl
+import openpyxl
 
+rank_list = []
 now_movie_list = []
 emotion_list = []
 jieba.add_word('任素汐')
+
+# Baidu_Aip密钥
 APP_ID = '14963270'
 API_KEY = 'DzcClKytSKKGpbTxdSFUcyif'
 SECRET_KEY = 'IIIiZbKyHdFDqA7xq17kb8wS3elRVF7a'
 
 
+# 模拟登录
 def login_douban():
     login_url = "https://accounts.douban.com/login"
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64)\
@@ -35,20 +41,24 @@ def login_douban():
     html = r.text
     if html.__contains__('try'):
         print(html)
+
+    # 如果直接登录成功则返回
     if r.url == 'https://movie.douban.com':
         return html
+    # 否则拉取captcha
     reg = r'<img id="captcha_image" src="(.*?)" alt="captcha" class="captcha_image"/>'
     imglist = re.findall(reg, html)
     pic = 'captchas\\%d.jpg' % random.randint(1,100)
     urllib.request.urlretrieve(imglist[0], pic)
     img = Image.open(pic)
+    # 调用plt在内部绘制captcha
     plt.figure("captcha")
     plt.imshow(img)
     plt.show()
+    # 手动输入captcha
     captcha = input('captcha is: ')
     regid = r'<input type="hidden" name="captcha-id" value="(.*?)"/>'
     ids = re.findall(regid, html)
-
     my_post["captcha-solution"] = captcha
     my_post["captcha-id"] = ids[0]
     q = requests.post(login_url, data=my_post, headers=headers)
@@ -56,13 +66,14 @@ def login_douban():
     return q.text
 
 
+# jieba分词
 def jieba_split(txt):
     text = open(txt, "rb").read()
     words_list = []
     word_generator = jieba.cut(text, cut_all=False)
+    # 加载停止词并去除
     with open('chineseStopWords.txt') as f:
         str_text = f.read()
-        # unicode_text = str_text.encode("utf-8")
         f.close()
     for word in word_generator:
         if word.strip() not in str_text:
@@ -70,19 +81,21 @@ def jieba_split(txt):
     return ' '.join(words_list)
 
 
+# 调用api分析评论情感
 def get_sentiments(text):
     try:
         sitems=client.sentimentClassify(text)['items'][0]# 情感分析
         print(sitems)
         print('\n')
-        positive = sitems['positive_prob']# 积极概率
-        confidence = sitems['confidence']# 置信度
-        sentiment = sitems['sentiment']# 0表示消极，1表示中性，2表示积极
+        # 积极概率 positive = sitems['positive_prob']
+        # 置信度 confidence = sitems['confidence']
+        # 0表示消极，1表示中性，2表示积极 sentiment = sitems['sentiment']
         return sitems
     except Exception as e:
         print(e)
 
 
+# 获取正在热映电影
 def get_now_movie(html):
     soup = BeautifulSoup(html)
     div_list = soup.body.find(id='screening')
@@ -95,7 +108,6 @@ def get_now_movie(html):
             pass
         else:
             now_movie_list.append(now_movie_dict)
-    # print(now_movie_list)
 
     for movie in now_movie_list:
         movie['name'] = re.findall(r'\w+', movie['name'])[0]
@@ -103,7 +115,8 @@ def get_now_movie(html):
         print(movie['name'] + '_' + movie['score'] + '分')
 
 
-def getcomment(comment_url_list, filename):
+# 根据短评页面url获取评论
+def getcomment(comment_url_list, filename, switch):
     comments_list = []
     n = 0
     sum = 0
@@ -111,33 +124,55 @@ def getcomment(comment_url_list, filename):
         comment_res = urllib.request.urlopen(url)
         comment_soup = BeautifulSoup(comment_res, 'html.parser')
         comments = [item.string for item in comment_soup.find_all('span', class_='short')]
-        # ranks = [item for item in comment_soup.findAll('span', class_='allstar40 rating')]
         comments_list.append(comments)
+        if switch:
+            # 导出评论及评分list
+            tags = [item for item in comment_soup.findAll('div', class_='comment')]
+            try:
+                for i in range(20):
+                    rank = {
+                        'comments': comments[i],
+                        # 使用正则表达式提取评分
+                        'rank': re.findall(r"allstar(.+?)0 rating", str(tags[i])),
+                    }
+                    # 只收集评分不为3且长度大于4的评论
+                    if rank['rank'] and int(rank['rank'][0]) != 3 and len(rank['comments']):
+                        print(rank)
+                        rank_list.append(rank)
+            except Exception as e:
+                print(e)
 
-    with open('comments\\'+ filename + '.txt', 'w', encoding='utf-8') as file:
-        print('\n'+filename+'\n')
-        for com_list in comments_list:
-            for com in com_list:
-                print(com)
-                pattern = re.compile(r'[\u4e00-\u9fa5]+')
-                filterdata = re.findall(pattern, str(com))
-                cleaned_comments = ''.join(filterdata)
-                sitems = get_sentiments(emoji.demojize(com))
-                try:
-                    sum = sum + sitems['positive_prob']*sitems['confidence']
-                    n = n + sitems['confidence']
-                except:
-                    print('pass')
-                file.write(cleaned_comments + '\n')
+    if not switch:
+        # 将分词结果写入文件
+        with open('comments\\'+ filename + '.txt', 'w', encoding='utf-8') as file:
+            print('\n'+filename+'\n')
+            for com_list in comments_list:
+                for com in com_list:
+                    print(com)
+                    # 过滤标点符号
+                    pattern = re.compile(r'[\u4e00-\u9fa5]+')
+                    filterdata = re.findall(pattern, str(com))
+                    cleaned_comments = ''.join(filterdata)
+                    # 获取情感分析并计算加权平均
+                    sitems = get_sentiments(emoji.demojize(com))
+                    try:
+                        sum = sum + sitems['positive_prob']*sitems['confidence']
+                        n = n + sitems['confidence']
+                    except:
+                        print('pass')
 
-    emotion = {'movie': filename,
-               'emotion': round(sum / n, 2),
-               }
-    print(emotion)
-    emotion_list.append(emotion)
-    create_wordcloud(jieba_split('comments\\' + filename + '.txt'), filename)
+                    file.write(cleaned_comments + '\n')
+
+        emotion = {'movie': filename,
+                   'emotion': round(sum / n, 2),
+                   }
+        print(emotion)
+        emotion_list.append(emotion)
+
+        create_wordcloud(jieba_split('comments\\' + filename + '.txt'), filename)
 
 
+# 生成词云
 def create_wordcloud(wl, filename):
     wc = WordCloud(background_color="white",
                    max_words=2000,
@@ -149,14 +184,48 @@ def create_wordcloud(wl, filename):
     try:
         myword = wc.generate(wl)
         wc.to_file('wordclouds\\' + filename + '.jpg')
+        # 自定义图片标题字体以显示中文
+        mpl.rcParams['font.sans-serif'] = ['SimHei']
         plt.imshow(myword)
+        plt.title(filename)
         plt.axis("off")
         plt.show()
     except BaseException as e:
         print(e)
 
 
+# 导出评论及星级到excel
+def ex_ranks():
+    comment_url_list = []
+    # 自定义爬取电影id
+    custom_id = ['26366496', '26752088', '27102569',
+                 '26363254', '20376577', '26850326',
+                 '27605698', '26985127', '26996640',
+                 '26698897', '25823277', '25911694',
+                 '26667056', '26753020', '1292213',
+                 '3742360', '26861685', '4920528',
+                 '26683723', '26575103', '30122633']
+    for i in range(len(custom_id)):
+        for page in range(11):
+            comment_url_list.append('https://movie.douban.com/subject/' + custom_id[i] + '/comments?start=' + str(
+                20 * page) + '&limit=20&sort=new_score&status=P')
+        getcomment(comment_url_list, 'custom', 1)
+        comment_url_list = []
+
+    # 导出短评及评分到excel
+    excel = openpyxl.Workbook()
+    ws1 = excel.worksheets[0]
+    ws1.title = u"Sheet1"
+    ws1.cell(row=1, column=1).value = 'comment'
+    ws1.cell(row=1, column=2).value = 'star'
+    for i in range(len(rank_list)):
+        ws1.cell(row=i+2, column=1).value = emoji.demojize(rank_list[i]['comments'])
+        ws1.cell(row=i+2, column=2).value = int(rank_list[i]['rank'][0])
+    excel.save("ranks.xlsx")
+
+
 if __name__ == '__main__':
+    # 建立comments, wordclouds, captchas文件夹以存放评论、词云和验证码
     if not os.path.exists('comments\\'):
         os.makedirs('comments\\')
     if not os.path.exists('wordclouds\\'):
@@ -164,10 +233,12 @@ if __name__ == '__main__':
     if not os.path.exists('captchas\\'):
         os.makedirs('captchas\\')
 
+    # 加载百度Ai自然语言处理
     client = AipNlp(APP_ID, API_KEY, SECRET_KEY)
 
     while True:
         html = login_douban()
+        time.sleep(1)
         if BeautifulSoup(html).title.get_text() == '登录豆瓣':
             print('登录失败，请等待3秒')
             time.sleep(3)
@@ -175,15 +246,17 @@ if __name__ == '__main__':
             print('登录成功')
             break
 
+    # 获取正在热映的电影list
     get_now_movie(html)
-
+    # 添加短评每页的url到comment_url_list
     comment_url_list = []
     for item in now_movie_list:
         for page in range(11):
             comment_url_list.append('https://movie.douban.com/subject/' + item['id'] + '/comments?start=' + str(
                 20 * page) + '&limit=20&sort=new_score&status=P')
-        getcomment(comment_url_list, item['name'] + '_'+ item['score'] + '分')
+        getcomment(comment_url_list, item['name'] + '_'+ item['score'] + '分', 0)
         comment_url_list = []
 
+    # 打印每部电影的情感分析评分（0为负面，1为正面）
     for i in emotion_list:
         print(i)
---
+++ emotion_analyse.py
@@ -0,0 +1,125 @@
+import pandas as pd
+import jieba
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import cross_val_score
+from sklearn import metrics
+import matplotlib.pyplot as plt
+from aip import AipNlp
+import emoji
+
+# Baidu_Aip密钥
+APP_ID = '14963270'
+API_KEY = 'DzcClKytSKKGpbTxdSFUcyif'
+SECRET_KEY = 'IIIiZbKyHdFDqA7xq17kb8wS3elRVF7a'
+# 加载百度Ai自然语言处理
+client = AipNlp(APP_ID, API_KEY, SECRET_KEY)
+
+# 评星数量>3作为正向情感，取值为1；反之视作负向情感，取值为0
+def make_label(df):
+    df["sentiment"] = df["star"].apply(lambda x: 1 if x>3 else 0)
+
+
+def chinese_word_cut(mytext):
+    return " ".join(jieba.cut(mytext))
+
+
+def get_custom_stopwords(stop_words_file):
+    with open(stop_words_file) as f:
+        stopwords = f.read()
+    stopwords_list = stopwords.split('\n')
+    custom_stopwords_list = [i for i in stopwords_list]
+    return custom_stopwords_list
+
+# 调用百度api情感分析
+def get_sentiment(text):
+        # print(emoji.demojize(text))
+        try:
+            sitems=client.sentimentClassify(emoji.demojize(text))['items'][0]
+            # print(sitems['positive_prob'])
+            return sitems['positive_prob']
+        except Exception as e:
+            print(e)
+
+
+# 模型优度的可视化展现
+def visualize(text, y_test, y_pred):
+    fpr, tpr, _ = metrics.roc_curve(y_test, y_pred, pos_label=1)
+    auc = metrics.auc(fpr, tpr)
+
+    # 中文和负号的正常显示
+    plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
+    plt.rcParams['axes.unicode_minus'] = False
+    # 设置绘图风格
+    plt.style.use('ggplot')
+
+    # 绘制ROC曲线
+    plt.plot(fpr, tpr, '')
+    # 绘制参考线
+    plt.plot((0, 1), (0, 1), 'r--')
+    # 添加文本注释
+    plt.text(0.5, 0.5, 'ROC=%.2f' % auc)
+    # 设置坐标轴标签和标题
+    plt.title(text + ' - AUC')
+    plt.xlabel('1-specificity')
+    plt.ylabel('Sensitivity')
+
+    # 去除图形顶部边界和右边界的刻度
+    plt.tick_params(top='off', right='off')
+    # 图形显示
+    plt.show()
+
+
+# 数据读入
+df = pd.read_excel('ranks.xlsx', sheetname = 0)
+make_label(df)
+# 把特征和标签拆开
+X = df[['comment']]
+y = df.sentiment
+# 每一行评论数据都进行分词
+X['cutted_comment'] = X.comment.apply(chinese_word_cut)
+
+# 拆分数据集合为训练集和测试集
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+# 处理中文停用词
+stop_words_file = "chineseStopWords.txt"
+stopwords = get_custom_stopwords(stop_words_file)
+
+# 生成特征向量并降维
+max_df = 0.8 # 在超过这一比例的文档中出现的关键词（过于平凡），去除掉。
+min_df = 3 # 在低于这一数量的文档中出现的关键词（过于独特），去除掉。
+vect = CountVectorizer(max_df = max_df,
+                       min_df = min_df,
+                       token_pattern=u'(?u)\\b[^\\d\\W]\\w+\\b',
+                       stop_words=frozenset(stopwords))
+
+term_matrix = pd.DataFrame(vect.fit_transform(X_train.cutted_comment).toarray(), columns=vect.get_feature_names())
+
+# 朴素贝叶斯分类
+nb = MultinomialNB()
+pipe = make_pipeline(vect, nb)
+print('Multinomial naive bayes_训练集交叉验证准确率：',
+      cross_val_score(pipe, X_train.cutted_comment, y_train, cv=5, scoring='accuracy').mean())
+
+# 拟合模型生成测试集
+pipe.fit(X_train.cutted_comment, y_train)
+y_pred = pipe.predict(X_test.cutted_comment)
+accuracy1 = metrics.accuracy_score(y_test, y_pred)
+print('Multinomial naive bayes_测试集预测准确率：', accuracy1)
+print('Multinomial naive bayes混淆矩阵：')
+print(metrics.confusion_matrix(y_test, y_pred))
+# 模型优度的可视化展现
+visualize('Multinomial naive bayes', y_test, y_pred)
+
+# 对比百度ai的情感分析
+y_pred_baidu = X_test.comment.apply(get_sentiment)
+print('waiting for baidu_ai analyse...')
+y_pred_baidu_normalized = y_pred_baidu.apply(lambda x: 1 if x>0.5 else 0)
+accuracy2 = metrics.accuracy_score(y_test, y_pred_baidu_normalized)
+print('baidu_ai_测试集预测准确率：',accuracy2)
+print('baidu_ai混淆矩阵：')
+print(metrics.confusion_matrix(y_test, y_pred_baidu_normalized))
+visualize('baidu_ai', y_test, y_pred_baidu_normalized)
+
